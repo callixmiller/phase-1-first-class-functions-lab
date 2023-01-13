a336a00e5adf5816624ff3874fd372d7318ce1cd
callixmiller <calmill413@gmail.com>
2023-01-13 13:48:54 -0500

    index.js all tests passing, over thought last problem

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+function returnFirstTwoDrivers() {
+    return drivers.slice(0,2);
+}
+
+function returnLastTwoDrivers() {
+    return drivers.slice(2);
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(fare) {
+    return function () {
+        return fare*fare;
+    }
+}
+
+function fareDoubler(fare2) {
+    return fare2*2;
+}
+
+function fareTripler(fare3) {
+    return fare3*3;
+}
+
+function selectDifferentDrivers(selectingDrivers, drivers) {
+    return drivers(selectingDrivers);
+}
